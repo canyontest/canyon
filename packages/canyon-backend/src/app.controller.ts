2334172e48de5b43599e75d55663074bba0b2e1c
Allen Zhang <37892968+zhangtao25@users.noreply.github.com>
2024-06-27 00:47:36 +0800

    upd

--- packages/canyon-backend/src/app.controller.ts
+++ packages/canyon-backend/src/app.controller.ts
@@ -1,13 +1,19 @@
 import { Controller, Get } from "@nestjs/common";
 import { AppService } from "./app.service";
+import {PrismaService} from "./prisma/prisma.service";
 
 @Controller()
 export class AppController {
-	constructor(private readonly appService: AppService) {}
+	constructor(
+    private readonly appService: AppService,
+    private readonly prisma: PrismaService
+  ) {}
 
 	@Get()
-	getHello(): string {
+	getHello() {
 		const a = "hha";
-		return this.appService.getHello();
+		return this.prisma.user.findMany({
+      where: {}
+    });
 	}
 }
--- packages/canyon-backend/src/app.module.ts
+++ packages/canyon-backend/src/app.module.ts
@@ -1,10 +1,17 @@
 import { Module } from "@nestjs/common";
 import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
-
+import {PrismaService} from "./prisma/prisma.service";
+import {PrismaModule} from "./prisma/prisma.module";
+import { ConfigModule } from '@nestjs/config';
 @Module({
-	imports: [],
+	imports: [
+    PrismaModule,
+    ConfigModule.forRoot({
+      envFilePath: './.[env]',
+    }),
+  ],
 	controllers: [AppController],
-	providers: [AppService],
+	providers: [AppService,PrismaService],
 })
 export class AppModule {}
