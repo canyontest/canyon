8140eb65fecb0cee6124557aaeb36aab1d8f1dd6
Allen Zhang (张涛) <wr_zhang25@163.com>
2024-09-20 14:16:24 +0800

    upd

--- packages/canyon-backend/src/index.ts
+++ packages/canyon-backend/src/index.ts
@@ -1,7 +1,59 @@
+import { apollo, gql } from "@elysiajs/apollo";
 import { Elysia } from "elysia";
+import { PrismaClient } from '@prisma/client'
+import path from "node:path";
+import dotenv from "dotenv";
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
+dotenv.config({
+  path: path.resolve(__dirname, "../../../.env"),
+});
 
-console.log(
-	`🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`,
-);
+const prisma = new PrismaClient()
+
+async function main() {
+  // ... you will write your Prisma Client queries here
+  const u = await prisma.user.findMany({
+    where:{}
+  })
+  console.log(u)
+}
+
+main()
+  .then(async () => {
+    await prisma.$disconnect()
+  })
+  .catch(async (e) => {
+    console.error(e)
+    await prisma.$disconnect()
+    process.exit(1)
+  })
+
+
+new Elysia()
+.use(
+  apollo({
+    typeDefs: gql`
+      type Book {
+        title: String
+        author: String
+      }
+
+      type Query {
+        books: [Book]
+      }
+    `,
+    resolvers: {
+      Query: {
+        books: () => {
+          return [
+            {
+              title: "Elysia",
+              author: "saltyAom",
+            },
+          ];
+        },
+      },
+    },
+  }),
+)
+.listen(8080);
